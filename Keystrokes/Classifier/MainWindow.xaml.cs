ddc45698598c5444de5396badee428b15aa61a1d
Patryk Milewski <mpatryk97@gmail.com>
2019-01-14 19:21:58 +0100

    Method fifth

--- Keystrokes/Classifier/MainWindow.xaml.cs
+++ Keystrokes/Classifier/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using Keystrokes;
 
@@ -88,6 +89,74 @@ namespace Classifier
             ),
             file
         );
+
+        private static List<(int Id, List<Keystroke>)> GetSamples_4(
+            string directory,
+            string pattern
+        ) => EnumerateSamples_4(directory, pattern).ToList();
+
+        private static IEnumerable<(int Id, List<Keystroke>)> EnumerateSamples_4(
+            string directory,
+            string pattern
+        )
+        {
+            var samples = new List<(int Id, List<Keystroke> Keystrokes)>();
+
+            foreach (string file in Directory.GetFiles(directory, pattern))
+            {
+                int Id = int.Parse(
+                    Path.GetFileNameWithoutExtension(file)
+                        .Substring(1, 2)
+                );
+                var Keystrokes = new List<Keystroke>();
+
+                foreach (string line in File.ReadLines(file))
+                    if (!string.IsNullOrWhiteSpace(line))
+                        Keystrokes.Add(Keystroke.FromString(line));
+
+                yield return (Id, Keystrokes);
+            }
+        }
+
+        private static List<(int Id, List<Keystroke>)> GetSamples_5(
+            string directory,
+            string pattern
+        ) => Task.Run(() => GetSamplesAsync(directory, pattern)).Result;
+
+        private static async Task<List<(int, List<Keystroke>)>> GetSamplesAsync(
+            string directory,
+            string pattern
+        )
+        {
+            string[] files = Directory.GetFiles(directory, pattern);
+            var tasks = new Task<(int, List<Keystroke>)>[files.Length];
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                int t = i;
+                tasks[i] = Task.Run(() =>
+                {
+                    int Id = int.Parse(
+                        Path.GetFileNameWithoutExtension(files[t])
+                            .Substring(1, 2)
+                    );
+                    var Keystrokes = new List<Keystroke>();
+
+                    foreach (string line in File.ReadLines(files[t]))
+                        if (!string.IsNullOrWhiteSpace(line))
+                            Keystrokes.Add(Keystroke.FromString(line));
+
+                    return (Id, Keystrokes);
+                });
+            }
+
+            var samples = new List<(int Id, List<Keystroke> Keystrokes)>();
+
+            foreach (var task in tasks)
+                samples.Add(await task);
+
+            return samples;
+        }
 
         private List<(int Id, List<Keystroke> Keystrokes)> Samples;
     }
