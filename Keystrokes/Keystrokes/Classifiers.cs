b86a85a60232e0c8fe5d8e76f81d435d96a82a79
Patryk Milewski <mpatryk97@gmail.com>
2019-01-14 13:32:38 +0100

    Unit Tests

--- Keystrokes/Keystrokes/Classifiers.cs
+++ Keystrokes/Keystrokes/Classifiers.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Keystrokes
 {
@@ -16,11 +13,5 @@ namespace Keystrokes
 
         public static int Chebyshev(int[] first, int[] second) =>
             first.Zip(second, (i, j) => i > j ? i - j : j - i).Max();
-
-        // Proceduralne
-        //int sum = 0;
-        //for (int i = 0; i < first.Length; i++)
-        //    sum += (first[i] - second[i]) * (first[i] - second[i]);
-        //return (int)Math.Round(Math.Sqrt(sum));
     }
 }
---
+++ Keystrokes/KeystrokesTests/ClassifiersTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Keystrokes.Tests
+{
+    [TestClass()]
+    public class ClassifiersTests
+    {
+        private readonly int[] first = { 15, 30, 20, 0 };
+        private readonly int[] second = { 5, 70, 25, 11 };
+
+        private void DistanceTest(Func<int[], int[], int> distance, int value)
+        {
+            Assert.AreEqual(distance(this.first, this.first), 0);
+            Assert.AreEqual(distance(this.first, this.second), value);
+            Assert.AreEqual(distance(this.second, this.first), value);
+            Assert.AreEqual(distance(this.second, this.second), 0);
+        }
+         
+        [TestMethod()]
+        public void EuclideanTest() => DistanceTest(Classifiers.Euclidean, 43);
+
+        [TestMethod()]
+        public void ManhattanTest() => DistanceTest(Classifiers.Manhattan, 66);
+
+        [TestMethod()]
+        public void ChebyshevTest() => DistanceTest(Classifiers.Chebyshev, 40);
+    }
+}
